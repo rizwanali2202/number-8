32d1174098b7f1ace5e8f383379bb9f9c285f598
Rizwan Ali <164035652+rizwanali2202@users.noreply.github.com>
2024-03-28 17:49:27 +0500

    Make 8 number using typescript program file
    By using this program you find the answer 8 using multiplication, division, addition and subtraction

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("sum =", 5 + 3);
+console.log("minus =", 11 - 3);
+console.log("divide =", 40 / 5);
+console.log("multiply =", 4 * 2);
---
+++ app.ts
@@ -0,0 +1,4 @@
+console.log("sum =", 5 + 3);
+console.log("minus =", 11 - 3);
+console.log("divide =",  40 / 5);
+console.log("multiply =", 4 * 2);
